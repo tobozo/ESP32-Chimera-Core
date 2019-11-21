e95c3b20fe9cef388d406ff000ebd768792d6f44
tobozo <tobozo@users.noreply.github.com>
2019-11-21 12:12:34 +0100

    Added destructor to Sprite

--- src/utility/Sprite.h
+++ src/utility/Sprite.h
@@ -10,7 +10,11 @@ class TFT_eSprite : public TFT_eSPI {
 
  public:
 
-  TFT_eSprite(TFT_eSPI *tft);
+  TFT_eSprite(TFT_eSPI *tft); // Constructor
+
+  ~TFT_eSprite() { // Destructor
+    deleteSprite();
+  }
 
            // Create a sprite of width x height pixels, return a pointer to the RAM area
            // Sketch can cast returned value to (uint16_t*) for 16 bit depth if needed
