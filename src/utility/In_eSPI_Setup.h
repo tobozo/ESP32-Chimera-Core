3a6b06e1e49135b5c8fcbd5814183799a94cf033
tobozo <tobozo@users.noreply.github.com>
2020-02-01 13:06:36 +0100

    Adding support for ESP32Marauder (LoLinD32Pro)

--- src/utility/In_eSPI_Setup.h
+++ src/utility/In_eSPI_Setup.h
@@ -1,6 +1,51 @@
 #define M5STACK
 
-#if defined( ARDUINO_DDUINO32_XS )
+
+#if defined( ARDUINO_LOLIN_D32_PRO )
+
+  #define ILI9341_DRIVER
+  #define TFT_DRIVER 0x9341
+/*
+  #define TFT_MISO 19 // Matching T_DO
+  #define TFT_MOSI 23 // Matching T_DIN
+  #define TFT_SCLK 18 // Matching T_CLK
+  #define TFT_CS   17  // Chip select control pin
+  #define TFT_DC   16  // Data Command control pin
+  #define TFT_RST   5  // Reset pin (could connect to RST pin)
+  //#define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
+*/
+  #include "TFT_Drivers/ILI9341_Defines.h"
+  #define TFT_BL   32  // LED back-light (only for ST7789 with backlight control pin)
+  #define TOUCH_CS 21     // Chip select pin (T_CS) of touch screen
+
+  #define TFT_WIDTH     240
+  #define TFT_HEIGHT    320
+
+  //#define TFT_INVERSION_OFF
+  //#undef TFT_MAD_BGR
+  //#define TFT_MAD_BGR 0
+
+
+  /*
+  #define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
+  #define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
+  #define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
+  #define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
+  #define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:-.
+  #define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
+  //#define LOAD_FONT8N // Font 8. Alternative to Font 8 above, slightly narrower, so 3 digits fit a 160 pixel TFT
+  #define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts*/
+
+  // this will save ~20kbytes of FLASH
+  #define SMOOTH_FONT
+
+  #define SPI_FREQUENCY   40000000 //5000000
+  #define SPI_READ_FREQUENCY  20000000
+  #define SPI_TOUCH_FREQUENCY  2500000
+
+
+
+#elif defined( ARDUINO_DDUINO32_XS )
   #define TFT_BL         22  // LED back-light
   #define TFT_DC         23
   #define TFT_RST        32
