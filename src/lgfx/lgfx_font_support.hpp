bf0697a326e8cefa494b0a904a594ee80060982d
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2020-05-26 19:52:46 +0900

    add drawBezier

--- src/lgfx/lgfx_font_support.hpp
+++ src/lgfx/lgfx_font_support.hpp
@@ -245,7 +245,7 @@ namespace lgfx
     void setTextDatum(textdatum_t datum) { _text_style.datum = datum; }
     [[deprecated("use textdatum_t")]]
     void setTextDatum(std::uint8_t datum) { _text_style.datum = (textdatum_t)datum; }
-    void setTextPadding(std::uint16_t padding_x) { _padding_x = padding_x; }
+    void setTextPadding(std::uint32_t padding_x) { _padding_x = padding_x; }
     void setTextWrap( bool wrapX, bool wrapY = false) { _textwrap_x = wrapX; _textwrap_y = wrapY; }
     void setTextScroll(bool scroll) { _textscroll = scroll; if (_cursor_x < this->_sx) { _cursor_x = this->_sx; } if (_cursor_y < this->_sy) { _cursor_y = this->_sy; } }
 
@@ -744,6 +744,7 @@ namespace lgfx
     std::int32_t _cursor_x = 0;
     std::int32_t _cursor_y = 0;
     std::int32_t _filled_x = 0;
+    std::int32_t _padding_x = 0;
 
     TextStyle _text_style;
     FontMetrics _font_metrics = { 6, 6, 0, 8, 8, 0, 7 }; // Font0 Metric
@@ -752,8 +753,6 @@ namespace lgfx
     RunTimeFont* _dynamic_font = nullptr;  // run-time generated font
     FileWrapper _font_file;
     PointerWrapper _font_data;
-
-    std::int16_t _padding_x = 0;
 
     bool _textwrap_x = true;
     bool _textwrap_y = false;
@@ -1089,7 +1088,7 @@ namespace lgfx
               if (flg || (fillbg && fill)) {
                 me->setRawColor(colortbl[flg]);
                 if (flg && x1 == std::int32_t((j-1)*sx)) ++x1;
-                me->writeFillRect(x + x0, y + y0, x1 - x0, std::max(1, y1 - y0));
+                me->writeFillRect(x + x0, y + y0, x1 - x0, std::max<std::int32_t>(1, y1 - y0));
               }
               x0 = x1;
               flg = !flg;
