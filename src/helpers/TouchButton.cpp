f3257a54e50c3f8ae74c146d2235430e99b74f10
tobozo <tobozo@users.noreply.github.com>
2021-07-01 13:45:35 +0200

    Enabling optional sprite as _gfx in Touch Buttons

--- src/helpers/TouchButton.cpp
+++ src/helpers/TouchButton.cpp
@@ -13,23 +13,43 @@ TouchButton::TouchButton(void) {
   _textdatum = MC_DATUM;
 }
 
+void TouchButton::initButton( TFT_eSPI *gfx, int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t outline, uint16_t fill, uint16_t textcolor, char *label, uint8_t textsize)
+{
+  isSprite = false;
+  initButton( (void*)gfx, x, y, w, h, outline, fill, textcolor, label, textsize);
+}
+
+void TouchButton::initButton( TFT_eSprite *gfx, int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t outline, uint16_t fill, uint16_t textcolor, char *label, uint8_t textsize)
+{
+  isSprite = true;
+  initButton( (void*)gfx, x, y, w, h, outline, fill, textcolor, label, textsize);
+}
+
+
+void TouchButton::initButtonUL( TFT_eSPI *gfx, int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t outline, uint16_t fill, uint16_t textcolor, char *label, uint8_t textsize)
+{
+  isSprite = false;
+  initButton( (void*)gfx, x, y, w, h, outline, fill, textcolor, label, textsize);
+}
+
+void TouchButton::initButtonUL( TFT_eSprite *gfx, int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t outline, uint16_t fill, uint16_t textcolor, char *label, uint8_t textsize)
+{
+  isSprite = true;
+  initButton( (void*)gfx, x, y, w, h, outline, fill, textcolor, label, textsize);
+}
+
+
 // Classic initButton() function: pass center & size
-void TouchButton::initButton(
- TFT_eSPI *gfx, int16_t x, int16_t y, uint16_t w, uint16_t h,
- uint16_t outline, uint16_t fill, uint16_t textcolor,
- char *label, uint8_t textsize)
+void TouchButton::initButton( void *gfx, int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t outline, uint16_t fill, uint16_t textcolor, char *label, uint8_t textsize)
 {
   // Tweak arguments and pass to the newer initButtonUL() function...
-  initButtonUL(gfx, x - (w / 2), y - (h / 2), w, h, outline, fill,
-    textcolor, label, textsize);
+  initButtonUL(gfx, x - (w / 2), y - (h / 2), w, h, outline, fill, textcolor, label, textsize);
 }
 
 // Newer function instead accepts upper-left corner & size
-void TouchButton::initButtonUL(
- TFT_eSPI *gfx, int16_t x1, int16_t y1, uint16_t w, uint16_t h,
- uint16_t outline, uint16_t fill, uint16_t textcolor,
- char *label, uint8_t textsize)
+void TouchButton::initButtonUL( void *gfx, int16_t x1, int16_t y1, uint16_t w, uint16_t h, uint16_t outline, uint16_t fill, uint16_t textcolor, char *label, uint8_t textsize)
 {
+  //isSprite = false;
   _x1           = x1;
   _y1           = y1;
   _w            = w;
@@ -53,38 +73,13 @@ void TouchButton::setLabelDatum(int16_t x_delta, int16_t y_delta, textdatum_t da
 
 
 void TouchButton::drawButton(bool inverted, String long_name) {
-  uint16_t fill, outline, text;
-
-  if(!inverted) {
-    fill    = _fillcolor;
-    outline = _outlinecolor;
-    text    = _textcolor;
+  if( isSprite ) {
+    auto gfx = reinterpret_cast<TFT_eSprite*>(_gfx);
+    drawButton( gfx, inverted, long_name );
   } else {
-    fill    = _textcolor;
-    outline = _outlinecolor;
-    text    = _fillcolor;
+    auto gfx = reinterpret_cast<TFT_eSPI*>(_gfx);
+    drawButton( gfx, inverted, long_name );
   }
-
-  uint8_t r = min(_w, _h) / 4; // Corner radius
-  _gfx->fillRoundRect(_x1, _y1, _w, _h, r, fill);
-  _gfx->drawRoundRect(_x1, _y1, _w, _h, r, outline);
-
-  _gfx->setTextColor(text, fill);
-  _gfx->setTextSize(_textsize);
-
-  textdatum_t tempdatum = _gfx->getTextDatum();
-  _gfx->setTextDatum(_textdatum);
-
-  //uint16_t tempPadding = _gfx->getTextPadding();
-  //_gfx->setTextPadding(0);
-
-  if (long_name == "")
-    _gfx->drawString(_label, _x1 + (_w/2) + _xd, _y1 + (_h/2) - 4 + _yd);
-  else
-    _gfx->drawString(long_name, _x1 + (_w/2) + _xd, _y1 + (_h/2) - 4 + _yd);
-
-  _gfx->setTextDatum(tempdatum);
-  //_gfx->setTextPadding(tempPadding);
 }
 
 
--- src/helpers/TouchButton.h
+++ src/helpers/TouchButton.h
@@ -18,31 +18,58 @@ class TouchButton {
  public:
   TouchButton(void);
   // "Classic" initButton() uses center & size
-  void     initButton(TFT_eSPI *gfx, int16_t x, int16_t y,
-                      uint16_t w, uint16_t h, uint16_t outline, uint16_t fill,
-                      uint16_t textcolor, char *label, uint8_t textsize);
-
+  void initButton(void *gfx, int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t outline, uint16_t fill, uint16_t textcolor, char *label, uint8_t textsize);
+  void initButton(TFT_eSPI *gfx, int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t outline, uint16_t fill, uint16_t textcolor, char *label, uint8_t textsize);
+  void initButton(TFT_eSprite *gfx, int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t outline, uint16_t fill, uint16_t textcolor, char *label, uint8_t textsize);
   // New/alt initButton() uses upper-left corner & size
-  void     initButtonUL(TFT_eSPI *gfx, int16_t x1, int16_t y1,
-                        uint16_t w, uint16_t h, uint16_t outline, uint16_t fill,
-                        uint16_t textcolor, char *label, uint8_t textsize);
-
+  void initButtonUL(void *gfx, int16_t x1, int16_t y1, uint16_t w, uint16_t h, uint16_t outline, uint16_t fill, uint16_t textcolor, char *label, uint8_t textsize);
+  void initButtonUL(TFT_eSPI *gfx, int16_t x1, int16_t y1, uint16_t w, uint16_t h, uint16_t outline, uint16_t fill, uint16_t textcolor, char *label, uint8_t textsize);
+  void initButtonUL(TFT_eSprite *gfx, int16_t x1, int16_t y1, uint16_t w, uint16_t h, uint16_t outline, uint16_t fill, uint16_t textcolor, char *label, uint8_t textsize);
   // Adjust text datum and x, y deltas
-  void     setLabelDatum(int16_t x_delta, int16_t y_delta, textdatum_t datum = MC_DATUM);
-
+  void setLabelDatum(int16_t x_delta, int16_t y_delta, textdatum_t datum = MC_DATUM);
   void drawButton(bool inverted = false, String long_name = "");
+
+  template<typename T> // using TFT or Sprite
+  void drawButton( T *gfx, bool inverted = false, String long_name = "") {
+    uint16_t fill, outline, text;
+
+    if(!inverted) {
+      fill    = _fillcolor;
+      outline = _outlinecolor;
+      text    = _textcolor;
+    } else {
+      fill    = _textcolor;
+      outline = _outlinecolor;
+      text    = _fillcolor;
+    }
+    uint8_t r = min(_w, _h) / 4; // Corner radius
+    gfx->fillRoundRect(_x1, _y1, _w, _h, r, fill);
+    gfx->drawRoundRect(_x1, _y1, _w, _h, r, outline);
+    gfx->setTextColor(text, fill);
+    gfx->setTextSize(_textsize);
+    textdatum_t tempdatum = gfx->getTextDatum();
+    gfx->setTextDatum(_textdatum);
+    //uint16_t tempPadding = _gfx->getTextPadding();
+    //_gfx->setTextPadding(0);
+    if (long_name == "")
+      gfx->drawString(_label, _x1 + (_w/2) + _xd, _y1 + (_h/2) - 4 + _yd);
+    else
+      gfx->drawString(long_name, _x1 + (_w/2) + _xd, _y1 + (_h/2) - 4 + _yd);
+    gfx->setTextDatum(tempdatum);
+    //_gfx->setTextPadding(tempPadding);
+  }
 
   //void drawButton(String label, bool inverted, uint16_t margin=22);
   //void drawButton(bool inverted = false);
-  bool  contains(int16_t x, int16_t y);
-
-  void     press(bool p);
-  bool  isPressed();
-  bool  justPressed();
-  bool  justReleased();
+  bool contains(int16_t x, int16_t y);
+  void press(bool p);
+  bool isPressed();
+  bool justPressed();
+  bool justReleased();
 
  private:
-  TFT_eSPI *_gfx;
+  bool isSprite = false;
+  void *_gfx; // using TFT or Sprite
   int16_t  _x1, _y1; // Coordinates of top-left corner
   int16_t  _xd, _yd; // Button text datum offsets (wrt center of button)
   uint16_t _w, _h;
