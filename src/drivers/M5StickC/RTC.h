d8d7b24974d9ea6595473c7516887afd9744d0b6
tobozo <tobozo@users.noreply.github.com>
2021-10-01 16:34:15 +0200

    typo fix with backwards compat (GetData/GetDate)

--- src/drivers/M5StickC/RTC.h
+++ src/drivers/M5StickC/RTC.h
@@ -19,7 +19,7 @@ typedef struct
   uint16_t Year;
 }RTC_DateTypeDef;
 
-class RTC { 
+class RTC {
 public:
   RTC();
 
@@ -30,7 +30,8 @@ public:
   void SetData(RTC_DateTypeDef* RTC_DateStruct);
 
   void GetTime(RTC_TimeTypeDef* RTC_TimeStruct);
-  void GetData(RTC_DateTypeDef* RTC_DateStruct); 
+  void GetData(RTC_DateTypeDef* RTC_DateStruct);
+  void GetDate(RTC_DateTypeDef* RTC_DateStruct) { GetData( RTC_DateStruct); } // fix typo (Data/Date) + backwards compat)
 public:
   uint8_t Second;
   uint8_t Minute;
@@ -44,7 +45,7 @@ public:
 
   uint8_t asc[14];
 
-  
+
 private:
   void Bcd2asc(void);
   void DataMask();
@@ -53,13 +54,13 @@ private:
 
   uint8_t Bcd2ToByte(uint8_t Value);
   uint8_t ByteToBcd2(uint8_t Value);
-   
+
 private:
 
   /*定义数组用来存储读取的时间数据 */
-  uint8_t trdata[7]; 
+  uint8_t trdata[7];
   /*定义数组用来存储转换的 asc 码时间数据*/
-  //uint8_t asc[14]; 
+  //uint8_t asc[14];
 
 };
 
