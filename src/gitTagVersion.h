4929def4f2e425cd75a1fe5a6f93bfb76464964c
tobozo <tobozo@users.noreply.github.com>
2021-10-15 21:05:54 +0200

    raising version

--- src/gitTagVersion.h
+++ src/gitTagVersion.h
@@ -1,7 +1,12 @@
 #define ECC_VERSION_MAJOR 1
 #define ECC_VERSION_MINOR 2
-#define ECC_VERSION_PATCH 3
+#define ECC_VERSION_PATCH 4
 #define _ECC_STR(x) #x
 #define ECC_STR(x) _ECC_STR(x)
+// Macro to convert library version number into an integer
+#define ECC_VERSION_VAL(major, minor, patch) ((major << 16) | (minor << 8) | (patch))
+// current library version as a string
 #define CHIMERA_CORE_VERSION ECC_STR(ECC_VERSION_MAJOR) "." ECC_STR(ECC_VERSION_MINOR) "." ECC_STR(ECC_VERSION_PATCH_
+// current library version as an int, to be used in comparisons, such as M5_SD_UPDATER_VERSION_INT >= VERSION_VAL(2, 0, 0)
+#define CHIMERA_CORE_VERSION_INT ECC_VERSION_VAL(ECC_VERSION_MAJOR, ECC_VERSION_MINOR, ECC_VERSION_PATCH)
 
