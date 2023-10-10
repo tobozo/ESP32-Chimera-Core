033fc23eb69456e38ede3caf61c49b58020c175d
tobozo <tobozo@users.noreply.github.com>
2023-10-10 19:30:22 +0200

    fix bad macros for 3.0.0-alpha

--- src/drivers/Odroid-Go/Battery/battery.h
+++ src/drivers/Odroid-Go/Battery/battery.h
@@ -12,36 +12,41 @@
   #include <esp_arduino_version.h>
 #endif
 
+#define HAS_ADC_CAL_SUPPORT
+
 #if defined ESP_ARDUINO_VERSION_VAL
-  #if ESP_ARDUINO_VERSION > ESP_ARDUINO_VERSION_VAL(2,0,14)
-    #include <driver/adc.h>
-    #include <esp_adc_cal.h>
-
-    #define BATTERY_RESISTANCE_NUM 2
-    #define BATTERY_SAMPLES 64
-    #define BATTERY_VMAX 420
-    #define BATTERY_VMIN 330
-    #define BATTERY_CUTOFF 325
-
-    class Battery
-    {
-    public:
-      Battery(void);
-
-      void begin();
-      double getVoltage();
-      int getPercentage();
-      void setProtection(bool enable);
-      void update();
-
-    private:
-      #if defined ESP_ARDUINO_VERSION_VAL
-      #if ESP_ARDUINO_VERSION < ESP_ARDUINO_VERSION_VAL(3,0,0)
-        esp_adc_cal_characteristics_t _adc_chars;
-      #endif
-      #endif
-      bool _enable_protection;
-    };
-
+   #if ESP_ARDUINO_VERSION > ESP_ARDUINO_VERSION_VAL(2,0,14)
+    #undef HAS_ADC_CAL_SUPPORT
   #endif
 #endif
+
+
+#include <driver/adc.h>
+#include <esp_adc_cal.h>
+
+#define BATTERY_RESISTANCE_NUM 2
+#define BATTERY_SAMPLES 64
+#define BATTERY_VMAX 420
+#define BATTERY_VMIN 330
+#define BATTERY_CUTOFF 325
+
+class Battery
+{
+public:
+  Battery(void);
+
+  void begin();
+  double getVoltage();
+  int getPercentage();
+  void setProtection(bool enable);
+  void update();
+
+private:
+  #if defined HAS_ADC_CAL_SUPPORT
+    esp_adc_cal_characteristics_t _adc_chars;
+  #endif
+  bool _enable_protection;
+};
+
+//   #endif
+// #endif
